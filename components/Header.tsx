136e09e5bfedc3d2dfbddf7611beb8c503db8804
evertonsilvagit <everton@evertonsilva.io>
2024-05-17 12:08:26 -0300

    Colocando imagem no path absoluto

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className="flex justify-center items-center h-20 shadow-sm">
       <img
         className="mx-40"
-        src="/logo-mrv.webp"
+        src="./logo-mrv.webp"
         height={32}
         width={113}
         alt="MRV Logo"
---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    outDir: 'export',
+}
+
+module.exports = nextConfig
