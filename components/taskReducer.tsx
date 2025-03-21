eaec9566c88417a00c1a70857a21620309193b05
evertonsilvagit <everton@evertonsilva.io>
2025-03-20 21:36:54 -0300

    merge

--- components/taskReducer.tsx
+++ components/taskReducer.tsx
@@ -1,36 +1,16 @@
 export default function taskReducer(tasks, action) {
     switch (action.type) {
-<<<<<<< HEAD
         case 'load': {
             return action.tasks
         }
         case 'adicionado' : {
 
             const nextId = tasks.length > 0 ? Math.max(...tasks.map(t => t.id)) + 1 : 1;
-=======
-        case 'listado' : {
-
-            return action.tasks.map((task) => {
-                return {
-                    id: task.id,
-                    isAlterar: false,
-                    isSelected: false,
-                    description: task.description
-                }
-            });
-            
-        }
-        case 'novaLinhaCriada' : {
->>>>>>> 72eba6d676848d918da55cee2f75317abd6c77b2
 
             return [
                 ...tasks,
                 {
-<<<<<<< HEAD
                     id: nextId,
-=======
-                    id: "",
->>>>>>> 72eba6d676848d918da55cee2f75317abd6c77b2
                     isAlterar: true,
                     isSelected: false,
                     description: "Insira um texto"
@@ -38,7 +18,6 @@ export default function taskReducer(tasks, action) {
             ]   
         }
         case 'alterado' : {
-<<<<<<< HEAD
             return tasks.map((t) => {
                                 
                 if(t.id === action.task.id) {
@@ -53,37 +32,6 @@ export default function taskReducer(tasks, action) {
                     .catch(() => console.log("Erro ao salvar a task com id " + action.task.id));
 
                     return action.task
-=======
-
-            if(action.task.id === "") {
-                fetch('http://localhost:8080/todo-list-mrv/api/v1/task', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(action.task)
-                });
-                console.log("Adicionado")
-            } else {
-                fetch('http://localhost:8080/todo-list-mrv/api/v1/task/' + action.task.id, {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(action.task)
-                });
-                console.log("Alterado")
-            }
-
-            return tasks.map((t) => { 
-                if(t.id === action.task.id) {
-                    return {
-                        isAlterar: action.task.isAlterar,
-                        description: action.task.description,
-                        id: action.task.id,
-                        isSelected: action.task.isSelected
-                    }
->>>>>>> 72eba6d676848d918da55cee2f75317abd6c77b2
                 } else {
                     return {
                         isAlterar: false,
