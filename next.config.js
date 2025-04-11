6fe70251da82da6416064f26dfd1cf878dff551c
evertonsilvagit <everton@evertonsilva.io>
2025-04-11 14:26:00 -0300

    refactoring PWS

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
-// next.config.js
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  cacheOnFrontendNav: true,
-  register: true,
-  aggressiveFrontEndNavCaching: true,
-  cacheStartUrl: true
-});
-
   /** @type {import('next').NextConfig} */
   const nextConfig = {
     reactStrictMode: true,
     swcMinify: true
   };
   
-  module.exports = withPWA(nextConfig);
+  module.exports = nextConfig;
 
   
